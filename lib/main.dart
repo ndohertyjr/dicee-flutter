1f63e8e4d26955ed8cb9d0cda1aae952cb039a8e
ndohertyjr <ndohertyjr@gmail.com>
2022-08-24 15:19:08 -0400

    Dice rolling app for flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 2;
+
+  void getRandomNum() {
+    leftDiceNum = Random().nextInt(6) + 1;
+    rightDiceNum = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        getRandomNum();
+                      });
+                    },
+                    child: Image(image: AssetImage('images/dice$leftDiceNum.png')))),
+            Expanded(
+                child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        getRandomNum();
+                      });
+                    },
+                    child: Image(image: AssetImage('images/dice$rightDiceNum.png'))))
+          ],
+        ),
+      ),
+    );
   }
 }
+
